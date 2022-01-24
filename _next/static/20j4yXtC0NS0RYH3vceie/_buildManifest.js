9387c12934c1c2b158c59be64c0dd03dbb0bcf26
nayunhwan <nayunhwan.dev@gmail.com>
2022-01-25 02:30:03 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/20j4yXtC0NS0RYH3vceie/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/901-36bcd6ca65703687.js","static/chunks/pages/index-6453150b2af3e7e9.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
